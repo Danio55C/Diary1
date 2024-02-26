439b409ff0d3d68064fffcd67a77d9cd3ab191a4
daniel Janca <jancadaniel55@gmail.com>
2024-02-26 14:05:52 +0100

    dodanie plików

---
+++ Diary1/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,91 @@
+﻿using Diary;
+using Diary1.Commands;
+using Diary1.Models;
+using Diary1.Properties;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+
+namespace Diary1.ViewModels
+{
+    public class SettingsViewModel : ViewModelBase
+    {
+        
+        public SettingsViewModel()
+        {
+            CloseCommand = new RelayCommand(Close);
+
+            ConfirmCommand = new RelayCommand(Confirm);
+
+            //UserDatabaseSettings userSettings = new UserDatabaseSettings
+            //{
+            //    ServerAdress = Settings.Default.ServerAdress,
+            //    ServerName = Settings.Default.ServerAdress,
+            //    DataBaseName = Settings.Default.ServerAdress,
+            //    DataBaseUser = Settings.Default.ServerAdress,
+            //    DataBasePassword = Settings.Default.ServerAdress,
+            //};
+
+
+        }
+
+        
+
+        public ICommand CloseCommand { get; set; }
+        public ICommand ConfirmCommand { get; set; }
+
+        private UserDatabaseSettings _settings;
+        
+        public UserDatabaseSettings UserSettings
+        {
+            get { return _settings; }
+            set { 
+                _settings = value;
+                OnPropertChanged();
+            }
+        }
+
+
+
+
+
+
+
+
+
+        private void Confirm(object obj)
+        {
+            Settings.Default.Save();
+            Process.Start(Application.ResourceAssembly.Location);
+            Application.Current.Shutdown();
+
+        }
+
+        private void Close(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Diary1/Views/Settings.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Diary1.Models.Domains;
+using Diary1.ViewModels;
+using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Diary1.Views
+{
+    /// <summary>
+    /// Interaction logic for Settings.xaml
+    /// </summary>
+    public partial class Settings : MetroWindow
+    {
+        public Settings()
+        {
+            InitializeComponent();
+            DataContext = new SettingsViewModel();
+        }
+    }
+}
